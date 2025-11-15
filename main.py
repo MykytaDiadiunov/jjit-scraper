d96f78eba0233edbaf64535071fcd9537b1ec8fc
MykytaDiadiunov <myk23ta@gmail.com>
2025-11-15 12:34:32 +0100

    feat: add some logging and fields to result

--- main.py
+++ main.py
@@ -23,28 +23,47 @@ def main():
                 offer_data = get_offer_data_by_url(url)
                 offers_data.append(offer_data)
             except Exception as e:
-                print(e)
+                print(f"main: {e}")
             finally:
-                time.sleep(2)
+                print("main: wait for 1s before next page")
+                time.sleep(1)
 
         offers_df = pd.DataFrame(offers_data)
 
         offers_df.to_excel(EXEL_PATH, index=False)
+        print("main: finished and creating offers Excel file")
 
 
 def get_offer_data_by_url(url: str) -> dict:
     offer_id = url.split(URL_BASE)[-1]
 
+    print(f"get_offer_data_by_url: starting scraping page with id: {offer_id}")
+
     response: requests.Response = requests.get(f"{BASE_API_URL}/{offer_id}")
     if response.status_code != 200:
-        raise Exception(f"url with id {offer_id} return a error {response.status_code}")
+        raise Exception(
+            f"url with id {offer_id} returned an error {response.status_code}"
+        )
 
     response_in_json = response.json()
 
-    return {
+    salary_data = response_in_json.get("employmentTypes")[0]
+
+    result = {
         "title": response_in_json.get("title"),
-        **response_in_json.get("employmentTypes")[0],
+        "experience_level": response_in_json.get("experienceLevel").get("label"),
+        "salary_by": salary_data.get("unit"),
+        "salary_value_from": salary_data.get("fromPln"),
+        "salary_value_to": salary_data.get("toPln"),
+        "contract_type": salary_data.get("label"),
+        "company_city": response_in_json.get("city"),
+        "working_time": response_in_json.get("workingTime").get("label"),
     }
+
+    print(
+        f"get_offer_data_by_url: completed scraping page with title: {result.get('title')}"
+    )
+    return result
 
 
 if __name__ == "__main__":
